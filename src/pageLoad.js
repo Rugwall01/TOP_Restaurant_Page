0a1b1c3879dadae3dca8fc56557826cf1f12b1d1
rugwall01 <161780368+Rugwall01@users.noreply.github.com>
2025-02-01 10:37:13 -0800

    edited class names for nav and nav button. Page load module underway.

---
+++ src/pageLoad.js
@@ -0,0 +1,20 @@
+
+const genHomePage = (function(){
+
+    const header = document.querySelector('.head');
+    const navBar = document.querySelector('.nav');
+    const navBtns = document.querySelectorAll('.navBtn');
+
+    const content = document.querySelector('#content');
+
+    const frontPage = document.createElement('div');
+    const searchBar = document.createElement('input');
+    const headline = document.createElement('h2');
+    const testamonial = document.createElement('h4');
+
+
+
+
+
+})();
+
