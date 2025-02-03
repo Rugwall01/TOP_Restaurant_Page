dcdaf37f6bbaf9d8a2cd488f0432f8c14b754b26
rugwall01 <161780368+Rugwall01@users.noreply.github.com>
2025-02-03 11:02:10 -0800

    added dynamic margins to bold1 and bold 2 so they stay on the testimonial card. Created menu module and wrote the initial menu items as objects in an array.

---
+++ src/menu.js
@@ -0,0 +1,15 @@
+
+
+export const menu = (function (){
+
+    const card = document.createElement('div');
+    const items = [
+        {name: "Twice-Cooked Pork", description: "Tender, juicy, smokey pork! Marinated in our house ponzu sauce and slow cooked for 16hrs!!", price: "$10.99"},
+        {name: "Chongqing Spicy Chicken", description: "Crispy, fiery, tender chicken! Marinated in our house spicy sauce and glazed with Chongqing finishing sauce.", price: "$8.99"},
+        {name: "Dan Dan Noodles", description: "Savory, nutty, spicy noodles! Marinated in our house spicy sauce and pan fried until crispy.", price: "$7.99"},
+        {name: "Mapo Tofu", description: "Silky, bold, tender tofu! Topped with spring onions, crispy fried onions, and sesame seeds. Drizzled with spicy GREEN and RED Szechuan Peppercorn Sauce.", price: "$7.99"},
+    ];
+
+
+
+})();
